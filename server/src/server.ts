7f77bbe088f29bfbfc8c810af318c5409ba62e91
SkyHeads <skyheads@live.com>
2020-12-04 19:57:05 -0300

    create appointment routes/initial logic/date-fns

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/', (request, response) => response.json({ ok: true }));
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333');
