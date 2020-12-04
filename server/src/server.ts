84e351176a4ba3599498f1c71d216ce741164683
SkyHeads <skyheads@live.com>
2020-12-04 19:03:34 -0300

    install eslint + prettier + editorconfig

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,9 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/', (request, response) => {
-  return response.json({ ok: true });
-});
+app.use('/', (request, response) => response.json({ ok: true }));
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333');
