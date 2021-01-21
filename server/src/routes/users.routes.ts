d35830cf084380c814c9915681f7e93684783a6e
SkyHeads <skyheads@live.com>
2021-01-21 18:20:17 -0300

    update user avatar service to update new avatar

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateAvatarUserService from '../services/UpdateUserAvatarService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
@@ -38,7 +39,20 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    return response.json({ ok: true });
+    try {
+      const updateAvatarService = new UpdateAvatarUserService();
+
+      const user = await updateAvatarService.execute({
+        user_id: request.user.id,
+        avatarFileName: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   },
 );
 
