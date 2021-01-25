09aec26ce5c7bfc47b97b990a6ecfd68319abf1f
SkyHeads <skyheads@live.com>
2021-01-25 03:02:20 -0300

    create global exception handler

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -15,23 +15,19 @@ usersRouter.get('/', ensureAuthenticated, async (request, response) => {
 });
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService();
+  const createUser = new CreateUserService();
 
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
-    });
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
 
-    delete user.password;
+  delete user.password;
 
-    return response.json(user);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json(user);
 });
 
 usersRouter.patch(
@@ -39,20 +35,16 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    try {
-      const updateAvatarService = new UpdateAvatarUserService();
+    const updateAvatarService = new UpdateAvatarUserService();
 
-      const user = await updateAvatarService.execute({
-        user_id: request.user.id,
-        avatarFileName: request.file.filename,
-      });
+    const user = await updateAvatarService.execute({
+      user_id: request.user.id,
+      avatarFileName: request.file.filename,
+    });
 
-      delete user.password;
+    delete user.password;
 
-      return response.json(user);
-    } catch (err) {
-      return response.status(400).json({ error: err.message });
-    }
+    return response.json(user);
   },
 );
 
