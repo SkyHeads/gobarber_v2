c85f0e024e9ab02de44a6f4ecc957ba88038c291
SkyHeads <skyheads@live.com>
2021-02-25 01:41:13 -0300

    new Route component/Redirect if isPrivate props

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
 
 import SignUp from '../pages/SignUp';
 import SignIn from '../pages/SignIn';
+
+import Dashboard from '../pages/Dashboard';
 
 const routes: React.FC = () => {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/sign-up" exact component={SignUp} />
+
+      <Route path="/dashboard" exact component={Dashboard} isPrivate />
     </Switch>
   );
 };
