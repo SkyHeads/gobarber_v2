f0dd8400bd0ebe5d027ad6e49d4ac0984aebf12b
SkyHeads <skyheads@live.com>
2021-04-07 17:21:43 -0300

    create appointments/users controllers

--- server/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ server/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,12 +1,10 @@
-import { parseISO } from 'date-fns';
 import { Router } from 'express';
-import { container } from 'tsyringe';
-
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import AppointmentsController from '../controllers/AppointmentsController';
 
 const appointmentsRouter = Router();
+const appointmentsController = new AppointmentsController();
 
 // appointmentsRouter.get('/', ensureAuthenticated, async (request, response) => {
 //   const appointments = await appointmentsRepository.find();
@@ -14,19 +12,10 @@ const appointmentsRouter = Router();
 //   return response.json(appointments);
 // });
 
-appointmentsRouter.post('/', ensureAuthenticated, async (request, response) => {
-  const { provider_id, date } = request.body;
-
-  const parsedDate = parseISO(date);
-
-  const createAppointment = container.resolve(CreateAppointmentService);
-
-  const appointment = await createAppointment.execute({
-    date: parsedDate,
-    provider_id,
-  });
-
-  return response.json(appointment);
-});
+appointmentsRouter.post(
+  '/',
+  ensureAuthenticated,
+  appointmentsController.create,
+);
 
 export default appointmentsRouter;
--- server/src/modules/users/infra/http/routes/users.routes.ts
+++ server/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,15 +1,17 @@
 import { Router } from 'express';
+
 import multer from 'multer';
 import uploadConfig from '@config/upload';
 
-import CreateUserService from '@modules/users/services/CreateUserService';
-import UpdateAvatarUserService from '@modules/users/services/UpdateUserAvatarService';
-
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
-import { container } from 'tsyringe';
+
+import UsersController from '../controllers/UsersController';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
+const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
 // usersRouter.get('/', ensureAuthenticated, async (request, response) => {
 //   const usersRepository = getRepository(User);
@@ -18,38 +20,13 @@ const upload = multer(uploadConfig);
 //   return response.json(users);
 // });
 
-usersRouter.post('/', async (request, response) => {
-  const { name, email, password } = request.body;
-
-  const createUser = container.resolve(CreateUserService);
-
-  const user = await createUser.execute({
-    name,
-    email,
-    password,
-  });
-
-  delete user.password;
-
-  return response.json(user);
-});
+usersRouter.post('/', usersController.create);
 
 usersRouter.patch(
   '/avatar',
   ensureAuthenticated,
   upload.single('avatar'),
-  async (request, response) => {
-    const updateAvatarService = container.resolve(UpdateAvatarUserService);
-
-    const user = await updateAvatarService.execute({
-      user_id: request.user.id,
-      avatarFileName: request.file.filename,
-    });
-
-    delete user.password;
-
-    return response.json(user);
-  },
+  userAvatarController.update,
 );
 
 export default usersRouter;
