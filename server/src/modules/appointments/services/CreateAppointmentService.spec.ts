4c41fda67e193b466f099fab556ea99d06ec5211
SkyHeads <skyheads@live.com>
2021-04-08 01:45:07 -0300

    all appointments services test done

--- server/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ server/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -17,22 +18,24 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe('121212');
   });
 
-  // it('should not be able to create two appointments in same time', async () => {
-  //   const fakeAppointmentsRepository = new FakeAppointmentsRepository();
-  //   const createAppointment = new CreateAppointmentService(
-  //     fakeAppointmentsRepository,
-  //   );
+  it('should not be able to create two appointments in same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
 
-  //   const appointment1 = await createAppointment.execute({
-  //     date: new Date(),
-  //     provider_id: '121212',
-  //   });
+    const appointmentDate = new Date(2021, 3, 8, 11);
 
-  //   const appointment2 = await createAppointment.execute({
-  //     date: new Date(),
-  //     provider_id: '121212',
-  //   });
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '121212',
+    });
 
-  //   expect(appointment1.date).toEqual(appointment2.date);
-  // });
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '121212',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
